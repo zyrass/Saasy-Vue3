740968d392a7f16aff231db1acb0c5ea7cbcfaac
Alain Guillon <zyrass@outlook.fr>
2024-02-08 17:35:45 +0100

    update MyNavbar
    Add missing divider

--- src/components/MyNavbar.vue
+++ src/components/MyNavbar.vue
@@ -2,12 +2,16 @@
     <nav
         class="flex justify-between p-8 border-y border-slate-200 *:transition-all items-center"
     >
-        <ul class="flex gap-x-4 hover:*:text-sky-500 font-bold cursor-pointer">
-            <li>Cashflow</li>
-            <li class="text-sky-500">Last 7 days</li>
-            <li>Last 30 days</li>
-            <li>All-time</li>
-        </ul>
+        <div
+            class="flex gap-x-4 hover:*:text-sky-500 font-bold cursor-pointer divide-x-2"
+        >
+            <span>Cashflow</span>
+            <ul class="flex gap-x-4">
+                <li class="text-sky-500 ml-4">Last 7 days</li>
+                <li>Last 30 days</li>
+                <li>All-time</li>
+            </ul>
+        </div>
         <button
             class="bg-sky-500 px-4 py-2 text-white capitalize rounded-md shadow-md hover:shadow-sm hover:bg-sky-700"
         >
