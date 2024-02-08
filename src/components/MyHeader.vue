f039224c362dc8887d9998bfe3d549890b25153f
Alain Guillon <zyrass@outlook.fr>
2024-02-08 17:31:56 +0100

    update MyHeader

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="h-16 grid grid-cols-3 items-center">
         <div
-            class="bg-[url('./assets/images/logo.png')] size-8 bg-cover ml-4"
+            class="bg-[url('./assets/images/logo.png')] size-8 bg-cover ml-8"
         ></div>
         <nav>
             <ul
@@ -13,11 +13,11 @@
                 <li>Expenses</li>
             </ul>
         </nav>
-        <ul class="flex justify-end mr-4 items-center gap-x-4">
+        <ul class="flex justify-end mr-8 items-center gap-x-4">
             <li><i class="fa-regular fa-bell text-xl text-slate-400"></i></li>
             <li>
                 <div
-                    class="bg-[url('https://randomuser.me/api/portraits/women/22.jpg')] size-8 bg-cover rounded-full"
+                    class="bg-[url('https://randomuser.me/api/portraits/women/22.jpg')] size-9 bg-cover rounded-full"
                 ></div>
             </li>
         </ul>
