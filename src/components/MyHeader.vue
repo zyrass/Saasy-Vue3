bc7311b752f4e6b4f428899021edd9eb082c7749
Alain Guillon <zyrass@outlook.fr>
2024-02-09 18:55:30 +0100

    add responsive MyHeader

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,9 +1,31 @@
 <template>
-    <header class="h-24 grid grid-cols-3 items-center">
+    <header class="h-24 grid grid-cols-3 items-center relative">
+        <div
+            class="flex sm:hidden ml-8 h-8 w-10 rounded-md shadow text-xl border items-center hover:bg-gray-200 active:bg-gray-300 justify-center cursor-pointer"
+            @click="updateState"
+        >
+            <i v-if="!state.openMenu" class="fa-solid fa-bars"></i>
+            <i v-if="state.openMenu" class="fa-solid fa-xmark"></i>
+
+            <!-- Menu exclusivement sur mobile -->
+            <div
+                v-if="state.openMenu"
+                class="absolute sm:hidden top-24 left-0 h-64 w-full bg-slate-100 border-b-2 border-t-1 border-gray-200 shadow-md transition-all"
+            >
+                <ul
+                    class="flex flex-col py-8 gap-y-4 items-center hover:*:text-sky-500 cursor-pointer transition-all *:py-1 font-bold"
+                >
+                    <li>Home</li>
+                    <li>Invoices</li>
+                    <li>Clients</li>
+                    <li>Expenses</li>
+                </ul>
+            </div>
+        </div>
         <div
             class="bg-[url('./assets/images/logo.png')] size-8 bg-cover ml-8"
         ></div>
-        <nav>
+        <nav class="hidden sm:block">
             <ul
                 class="flex justify-center hover:*:text-sky-500 cursor-pointer transition-all *:px-2 font-bold"
             >
@@ -24,6 +46,23 @@
     </header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive, watch } from 'vue';
+
+const state = reactive({
+    openMenu: false,
+});
+const updateState = () => {
+    state.openMenu = !state.openMenu;
+};
+
+// Ajout d'un watcher pour rafraîchir automatiquement lorsque state.openMenu change
+watch(
+    () => state.openMenu,
+    () => {
+        console.log('La valeur de openMenu a changé :', state.openMenu);
+    },
+);
+</script>
 
 <styles scoped></styles>
--- src/components/MyNavbar.vue
+++ src/components/MyNavbar.vue
@@ -3,10 +3,12 @@
         class="flex sm:flex-row flex-col justify-between py-8 sm:p-8 border-y border-slate-200 *:transition-all items-center"
     >
         <div
-            class="flex flex-col items-center pt-8 sm:pt-0 sm:flex-row gap-y-4 sm:order-1 order-2 sm:gap-x-4 hover:*:text-sky-500 font-bold cursor-pointer sm:divide-x-2"
+            class="flex flex-col items-center pt-8 sm:pt-0 sm:flex-row gap-y-4 sm:order-1 order-2 sm:gap-x-4 font-bold cursor-pointer sm:divide-x-2"
         >
-            <span>Cashflow</span>
-            <ul class="flex flex-col sm:flex-row sm:gap-x-4 gap-4 items-center">
+            <span class="hover:text-sky-500">Cashflow</span>
+            <ul
+                class="flex flex-col sm:flex-row sm:gap-x-4 gap-4 items-center hover:*:text-sky-500"
+            >
                 <li class="text-sky-500 sm:ml-4">Last 7 days</li>
                 <li>Last 30 days</li>
                 <li>All-time</li>
